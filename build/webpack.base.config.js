5e2fb016452a13d054d99a6016961c0b47623467
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-06-24 18:38:36 +0300

    built demo page

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
-  root: __dirname,
+  root: path.join(__dirname, '../'),
   src: path.join(__dirname, '../src'),
   dist: path.join(__dirname, '../dist'),
   assets: 'assets/'
@@ -23,7 +23,7 @@ module.exports = {
   },
   output: {
     filename: `${PATHS.assets}js/[name].js`,
-    path: PATHS.dist,
+    path: PATHS.root,
     publicPath: '/'
   },
   module: {
