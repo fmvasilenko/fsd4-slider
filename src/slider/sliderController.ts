7173ad8e632a3a9dedba4642c8df6eda934314b5
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-07-16 18:59:15 +0300

    fixed code style with eslint

--- src/slider/sliderController.ts
+++ src/slider/sliderController.ts
@@ -1,35 +1,40 @@
-///<reference path="./slider.d.ts" />
+/// <reference path='./slider.d.ts' />
 
-import { SliderView } from "./view/sliderView"
-import { SliderModel } from "./sliderModel"
-import { SliderConfig } from "./sliderConfig/sliderConfig"
-import { SliderState } from "./sliderState/sliderState"
+import { SliderView } from './view/sliderView';
+import { SliderModel } from './sliderModel';
+import { SliderConfig } from './sliderConfig/sliderConfig';
+import { SliderState } from './sliderState/sliderState';
 
 class SliderController {
-  private ROOT: HTMLElement
-  private config: SliderConfig
-  private state: SliderState
-  private MODEL: SliderModel
-  private VIEW: SliderView
-  private slide: SliderCallBackFunction
+  private ROOT: HTMLElement;
+
+  private config: SliderConfig;
+
+  private state: SliderState;
+
+  private MODEL: SliderModel;
+
+  private VIEW: SliderView;
+
+  private slide: SliderCallBackFunction;
 
   constructor(root: HTMLElement, config: SliderConfig, slide?: SliderCallBackFunction) {
-    this.ROOT = root
-    this.config = config
-    this.state = new SliderState()
-    this.MODEL = new SliderModel(this.config, this.state)
-    this.VIEW = new SliderView(this.ROOT, this.config, this.state)
-    this.slide = slide ? slide : () => {}
+    this.ROOT = root;
+    this.config = config;
+    this.state = new SliderState();
+    this.MODEL = new SliderModel(this.config, this.state);
+    this.VIEW = new SliderView(this.ROOT, this.config, this.state);
+    this.slide = slide || (() => {});
 
-    this.config.leftHandleValue.addSubscriber(this.slideFunction.bind(this))
-    this.config.rightHandleValue.addSubscriber(this.slideFunction.bind(this))
+    this.config.leftHandleValue.addSubscriber(this.slideFunction.bind(this));
+    this.config.rightHandleValue.addSubscriber(this.slideFunction.bind(this));
   }
 
   private slideFunction() {
-    let leftHandleValue = this.config.leftHandleValue.get() as number
-    let rightHandleValue = this.config.rightHandleValue.get() as number
-    this.slide(leftHandleValue, rightHandleValue)
+    const leftHandleValue = this.config.leftHandleValue.get() as number;
+    const rightHandleValue = this.config.rightHandleValue.get() as number;
+    this.slide(leftHandleValue, rightHandleValue);
   }
 }
 
-export { SliderController }
+export { SliderController };
