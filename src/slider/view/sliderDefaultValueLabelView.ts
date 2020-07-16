f4dbcfdc7b888495a970174605cb0c4e9085ee29
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-07-16 17:34:11 +0300

    fixed code style with eslint

--- src/slider/view/sliderDefaultValueLabelView.ts
+++ src/slider/view/sliderDefaultValueLabelView.ts
@@ -1,100 +1,112 @@
-import { SliderConfig } from "../sliderConfig/sliderConfig"
-import { SliderState } from "../sliderState/sliderState"
+import { SliderConfig } from '../sliderConfig/sliderConfig';
+import { SliderState } from '../sliderState/sliderState';
 
 class SliderDefaultValueLabel {
-  private CONTAINER: HTMLElement
-  private config: SliderConfig
-  private state: SliderState
-  private CLASSES: SliderClasses
-  private ROOT: HTMLElement
-  private LABEL: HTMLElement
-  private index: number
+  private CONTAINER: HTMLElement;
 
-  constructor(container: HTMLElement, config: SliderConfig, state: SliderState, index: number) {
-    this.CONTAINER = container
-    this.config = config
-    this.state = state
-    this.index = index
-    this.CLASSES = require("../sliderClasses.json")
-    this.ROOT = this.createRootElement()
-    this.LABEL = this.createLabel()
+  private config: SliderConfig;
 
-    this.updateValue()
-    this.updateShift()
-    this.switch()
+  private state: SliderState;
 
-    this.config.defaultValues.addSubscriber(this.updateValue.bind(this))
-    this.config.defaultValues.addSubscriber(this.updateShift.bind(this))
-    this.config.hasDefaultValues.addSubscriber(this.switch.bind(this))
-    this.config.isVertical.addSubscriber(this.switchVertical.bind(this))
-    this.config.isVertical.addSubscriber(this.updateShift.bind(this))
+  private CLASSES: SliderClasses;
 
-    this.bindEventListeners()
+  private ROOT: HTMLElement;
+
+  private LABEL: HTMLElement;
+
+  private index: number;
+
+  constructor(
+    container: HTMLElement,
+    config: SliderConfig,
+    state: SliderState,
+    index: number,
+  ) {
+    this.CONTAINER = container;
+    this.config = config;
+    this.state = state;
+    this.index = index;
+    this.CLASSES = require('../sliderClasses.json');
+    this.ROOT = this.createRootElement();
+    this.LABEL = this.createLabel();
+
+    this.updateValue();
+    this.updateShift();
+    this.switch();
+
+    this.config.defaultValues.addSubscriber(this.updateValue.bind(this));
+    this.config.defaultValues.addSubscriber(this.updateShift.bind(this));
+    this.config.hasDefaultValues.addSubscriber(this.switch.bind(this));
+    this.config.isVertical.addSubscriber(this.switchVertical.bind(this));
+    this.config.isVertical.addSubscriber(this.updateShift.bind(this));
+
+    this.bindEventListeners();
+  }
+
+  public remove() {
+    this.ROOT.remove();
   }
 
   private createRootElement(): HTMLElement {
-    let root = document.createElement("div")
+    const root = document.createElement('div');
 
-    root.classList.add(this.CLASSES.DEFAULT_VALUE)
-    if (this.config.isVertical.get() === true) root.classList.add(this.CLASSES.DEFAULT_VALUE_VERTICAL)
+    root.classList.add(this.CLASSES.DEFAULT_VALUE);
+    if (this.config.isVertical.get() === true) root.classList.add(this.CLASSES.DEFAULT_VALUE_VERTICAL);
 
-    return root
+    return root;
   }
 
   private createLabel(): HTMLElement {
-    let label = document.createElement("div")
+    const label = document.createElement('div');
 
-    label.classList.add(`${this.CLASSES.DEFAULT_VALUE_LABEL}`)
-    if (this.config.isVertical.get() === true) label.classList.add(`${this.CLASSES.DEFAULT_VALUE_LABEL_VERTICAL}`)
-    
-    this.ROOT.appendChild(label)
-    return label
+    label.classList.add(`${this.CLASSES.DEFAULT_VALUE_LABEL}`);
+    if (this.config.isVertical.get() === true) label.classList.add(`${this.CLASSES.DEFAULT_VALUE_LABEL_VERTICAL}`);
+
+    this.ROOT.appendChild(label);
+    return label;
   }
 
   private updateShift() {
-    let shift = 100 * this.index / ((this.config.defaultValues.get() as number[] | string[]).length - 1)
+    const defaultValues = this.config.defaultValues.get() as number[] | string[];
+    const shift = (100 * this.index) / (defaultValues.length - 1);
 
-    if (this.config.isVertical.get() === true) this.ROOT.style.bottom = `${shift}%`
-    else this.ROOT.style.left = `${shift}%`
+    if (this.config.isVertical.get() === true) this.ROOT.style.bottom = `${shift}%`;
+    else this.ROOT.style.left = `${shift}%`;
   }
 
   private updateValue() {
-    this.LABEL.innerHTML = `${(this.config.defaultValues.get() as number[] | string[])[this.index]}`
+    const defaultValues = this.config.defaultValues.get() as number[] | string[];
+    this.LABEL.innerHTML = `${defaultValues[this.index]}`;
   }
 
   private switch() {
-    if (this.config.hasDefaultValues.get() === true) this.CONTAINER.appendChild(this.ROOT)
-    else this.ROOT.remove()
+    if (this.config.hasDefaultValues.get() === true) this.CONTAINER.appendChild(this.ROOT);
+    else this.ROOT.remove();
   }
 
   private switchVertical(value: boolean) {
     if (value === true) {
-      this.ROOT.style.left = ""
-      this.ROOT.classList.add(this.CLASSES.DEFAULT_VALUE_VERTICAL)
-      this.LABEL.classList.add(this.CLASSES.DEFAULT_VALUE_LABEL_VERTICAL)
+      this.ROOT.style.left = '';
+      this.ROOT.classList.add(this.CLASSES.DEFAULT_VALUE_VERTICAL);
+      this.LABEL.classList.add(this.CLASSES.DEFAULT_VALUE_LABEL_VERTICAL);
+    } else {
+      this.ROOT.style.top = '';
+      this.ROOT.classList.remove(this.CLASSES.DEFAULT_VALUE_VERTICAL);
+      this.LABEL.classList.remove(this.CLASSES.DEFAULT_VALUE_LABEL_VERTICAL);
     }
-    else {
-      this.ROOT.style.top = ""
-      this.ROOT.classList.remove(this.CLASSES.DEFAULT_VALUE_VERTICAL)
-      this.LABEL.classList.remove(this.CLASSES.DEFAULT_VALUE_LABEL_VERTICAL)
-    }
-  }
-
-  public remove() {
-    this.ROOT.remove()
   }
 
   private bindEventListeners() {
-    this.ROOT.addEventListener("click", this.clickHandler.bind(this))
+    this.ROOT.addEventListener('click', this.clickHandler.bind(this));
   }
 
   private clickHandler() {
     if (this.config.isRange.get() === false) {
-      let defaultValues = this.config.defaultValues.get() as number[] | string[]
-      let position = this.index / (defaultValues.length - 1)
-      this.state.leftHandlePosition.set(position)
+      const defaultValues = this.config.defaultValues.get() as number[] | string[];
+      const position = this.index / (defaultValues.length - 1);
+      this.state.leftHandlePosition.set(position);
     }
   }
 }
 
-export { SliderDefaultValueLabel }
+export { SliderDefaultValueLabel };
