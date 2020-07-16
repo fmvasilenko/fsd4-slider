a495a2fe4e6d649d438029fedd2f96db3612ddac
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-07-16 17:18:49 +0300

    checked sliderConfig with eslint

--- src/slider/sliderConfig/sliderConfig.ts
+++ src/slider/sliderConfig/sliderConfig.ts
@@ -1,47 +1,58 @@
-import { SliderConfigItem } from "./sliderConfigItem"
+import { SliderConfigItem } from './sliderConfigItem';
 
 interface ImportedSliderConfig {
-  isRange?: boolean
-  hasDefaultValues?: boolean
-  isVertical?: boolean
-  valueLabelDisplayed?: boolean
-  limitsDisplayed?: boolean
-  minValue?: number
-  maxValue?: number
-  step?: number
-  leftHandleValue?: number
-  rightHandleValue?: number
-  defaultValues?: number[] | string[]
+  isRange?: boolean;
+  hasDefaultValues?: boolean;
+  isVertical?: boolean;
+  valueLabelDisplayed?: boolean;
+  limitsDisplayed?: boolean;
+  minValue?: number;
+  maxValue?: number;
+  step?: number;
+  leftHandleValue?: number;
+  rightHandleValue?: number;
+  defaultValues?: number[] | string[];
 }
 
 class SliderConfig {
-  public isRange: SliderConfigItem
-  public hasDefaultValues: SliderConfigItem
-  public isVertical: SliderConfigItem
-  public valueLabelDisplayed: SliderConfigItem
-  public limitsDisplayed: SliderConfigItem
-  public minValue: SliderConfigItem
-  public maxValue: SliderConfigItem
-  public step: SliderConfigItem
-  public leftHandleValue: SliderConfigItem
-  public rightHandleValue: SliderConfigItem
-  public defaultValues: SliderConfigItem
+  public isRange: SliderConfigItem;
+
+  public hasDefaultValues: SliderConfigItem;
+
+  public isVertical: SliderConfigItem;
+
+  public valueLabelDisplayed: SliderConfigItem;
+
+  public limitsDisplayed: SliderConfigItem;
+
+  public minValue: SliderConfigItem;
+
+  public maxValue: SliderConfigItem;
+
+  public step: SliderConfigItem;
+
+  public leftHandleValue: SliderConfigItem;
+
+  public rightHandleValue: SliderConfigItem;
+
+  public defaultValues: SliderConfigItem;
 
   constructor(importedConfig?: ImportedSliderConfig) {
-    let config = Object.assign(this.getDefaultConfig(), importedConfig)
-    this.isRange = this.setIsRange(config.isRange)
-    this.hasDefaultValues = this.setHasDefaultValues(config.hasDefaultValues)
-    this.isVertical = this.boolean(config.isVertical)
-    this.valueLabelDisplayed = this.boolean(config.valueLabelDisplayed)
-    this.limitsDisplayed = this.setLimitsDisplayed(config.limitsDisplayed)
-    this.minValue = this.setMinValue(config.minValue)
-    this.maxValue = this.setMaxValue(config.maxValue)
-    this.step = this.setStep(config.step)
-    this.defaultValues = this.setDefaultValues(config.defaultValues)
-    this.leftHandleValue = this.setLeftHandleValue(config.leftHandleValue)
-    this.rightHandleValue = this.setRightHandleValue(config.rightHandleValue)
+    const config = Object.assign(this.getDefaultConfig(), importedConfig);
+    this.isRange = this.setIsRange(config.isRange);
+    this.hasDefaultValues = this.setHasDefaultValues(config.hasDefaultValues);
+    this.isVertical = this.setBoolean(config.isVertical);
+    this.valueLabelDisplayed = this.setBoolean(config.valueLabelDisplayed);
+    this.limitsDisplayed = this.setLimitsDisplayed(config.limitsDisplayed);
+    this.minValue = this.setMinValue(config.minValue);
+    this.maxValue = this.setMaxValue(config.maxValue);
+    this.step = this.setStep(config.step);
+    this.defaultValues = this.setDefaultValues(config.defaultValues);
+    this.leftHandleValue = this.setLeftHandleValue(config.leftHandleValue);
+    this.rightHandleValue = this.setRightHandleValue(config.rightHandleValue);
   }
 
+  // eslint-disable-next-line class-methods-use-this
   private getDefaultConfig() {
     return {
       isRange: false,
@@ -52,136 +63,154 @@ class SliderConfig {
       minValue: 0,
       maxValue: 100,
       step: 1,
-      defaultValues: ["first", "second", "third"],
+      defaultValues: ['first', 'second', 'third'],
       leftHandleValue: 20,
-      rightHandleValue: 80
-    }
+      rightHandleValue: 80,
+    };
   }
 
-  private boolean(value: boolean): SliderConfigItem {
-    return new SliderConfigItem(value)
+  // eslint-disable-next-line class-methods-use-this
+  private setBoolean(value: boolean): SliderConfigItem {
+    return new SliderConfigItem(value);
   }
 
   private setIsRange(value: boolean): SliderConfigItem {
-    let isRange = new SliderConfigItem(value)
-    isRange.addSubscriber(this.updateHandlesValues.bind(this))
-    return isRange
+    const isRange = new SliderConfigItem(value);
+    isRange.addSubscriber(this.updateHandlesValues.bind(this));
+    return isRange;
   }
 
   private setHasDefaultValues(value: boolean): SliderConfigItem {
-    let hasDefaultValues = new SliderConfigItem(value, this.checkHasDefaultValues.bind(this))
-    hasDefaultValues.addSubscriber(this.updateHandlesValues.bind(this))
-    return hasDefaultValues
+    const hasDefaultValues = new SliderConfigItem(
+      value,
+      this.checkHasDefaultValues.bind(this),
+    );
+    hasDefaultValues.addSubscriber(this.updateHandlesValues.bind(this));
+    return hasDefaultValues;
   }
 
-  private setLimitsDisplayed(value: boolean): SliderConfigItem {
-    if (this.hasDefaultValues.get() === true) value = false
-    let limitsDisplayed = new SliderConfigItem(value, this.checkLimitsDisplayed.bind(this))
-    return limitsDisplayed
+  private setLimitsDisplayed(givenValue: boolean): SliderConfigItem {
+    const value = this.hasDefaultValues.get() === true
+      ? false
+      : givenValue;
+
+    return new SliderConfigItem(
+      value,
+      this.checkLimitsDisplayed.bind(this),
+    );
   }
 
   private setMinValue(value: number): SliderConfigItem {
-    let minValue = new SliderConfigItem(value, this.checkMinValue.bind(this))
-    minValue.addSubscriber(this.updateHandlesValues.bind(this))
-    return minValue
+    const minValue = new SliderConfigItem(value, this.checkMinValue.bind(this));
+    minValue.addSubscriber(this.updateHandlesValues.bind(this));
+    return minValue;
   }
 
-  private setMaxValue(value: number): SliderConfigItem {
-    if (value < this.minValue.get()) value = this.minValue.get() as number
-    let maxValue = new SliderConfigItem(value, this.checkMaxValue.bind(this))
-    maxValue.addSubscriber(this.updateHandlesValues.bind(this))
-    return maxValue
+  private setMaxValue(givenValue: number): SliderConfigItem {
+    const value = givenValue < this.minValue.get()
+      ? this.minValue.get() as number
+      : givenValue;
+
+    const maxValue = new SliderConfigItem(value, this.checkMaxValue.bind(this));
+    maxValue.addSubscriber(this.updateHandlesValues.bind(this));
+    return maxValue;
   }
 
-  private setStep(step: number): SliderConfigItem {
-    if (step < 1) step = 1
-    return new SliderConfigItem(step, this.checkStep.bind(this))
+  private setStep(givenStep: number): SliderConfigItem {
+    const step = this.checkStep(givenStep);
+    return new SliderConfigItem(step, this.checkStep.bind(this));
   }
 
   private setDefaultValues(values: number[] | string[]): SliderConfigItem {
-    let defaultValues =  new SliderConfigItem(values)
-    defaultValues.addSubscriber(this.updateHandlesValues.bind(this))
-    return defaultValues
+    const defaultValues = new SliderConfigItem(values);
+    defaultValues.addSubscriber(this.updateHandlesValues.bind(this));
+    return defaultValues;
   }
 
-  private setLeftHandleValue(value: number): SliderConfigItem {
-    value = this.checkHandleValue(value)
-    return new SliderConfigItem(value, this.checkLeftHandleValue.bind(this))
+  private setLeftHandleValue(givenValue: number): SliderConfigItem {
+    const value = this.checkHandleValue(givenValue);
+    return new SliderConfigItem(value, this.checkLeftHandleValue.bind(this));
   }
 
-  private setRightHandleValue(value: number): SliderConfigItem {
-    value = this.checkRightHandleValue(value)
-    return new SliderConfigItem(value, this.checkRightHandleValue.bind(this))
+  private setRightHandleValue(givenValue: number): SliderConfigItem {
+    const value = this.checkRightHandleValue(givenValue);
+    return new SliderConfigItem(value, this.checkRightHandleValue.bind(this));
   }
 
   private updateHandlesValues() {
-    this.leftHandleValue.set(this.leftHandleValue.get())
-    this.rightHandleValue.set(this.rightHandleValue.get())
-    this.leftHandleValue.set(this.leftHandleValue.get())
+    this.leftHandleValue.set(this.leftHandleValue.get());
+    this.rightHandleValue.set(this.rightHandleValue.get());
+    this.leftHandleValue.set(this.leftHandleValue.get());
   }
 
   private checkHasDefaultValues(value: boolean): boolean {
-    if (value === true) this.limitsDisplayed.set(false)
-    return value
+    if (value === true) this.limitsDisplayed.set(false);
+    return value;
   }
 
   private checkLimitsDisplayed(value: boolean): boolean {
-    if (value === true) this.hasDefaultValues.set(false)
-    return value
+    if (value === true) this.hasDefaultValues.set(false);
+    return value;
   }
 
-  private checkMinValue(value: number): number {
-    if (value > this.maxValue.get()) value = this.maxValue.get() as number
-    return value
+  private checkMinValue(givenValue: number): number {
+    return givenValue > this.maxValue.get()
+      ? (this.maxValue.get() as number)
+      : givenValue;
   }
 
-  private checkMaxValue(value: number): number {
-    if (value < this.minValue.get()) value = this.minValue.get() as number
-    return value
+  private checkMaxValue(givenValue: number): number {
+    return givenValue < this.minValue.get()
+      ? (this.minValue.get() as number)
+      : givenValue;
   }
 
-  private checkStep(step: number): number {
-    if (step < 1) step = 1
-    return step
+  // eslint-disable-next-line class-methods-use-this
+  private checkStep(givenStep: number): number {
+    return givenStep >= 1 ? givenStep : 1;
   }
 
-  private checkLeftHandleValue(value: number): number {
-    value = this.checkHandleValue(value)
+  private checkLeftHandleValue(givenValue: number): number {
+    let value = this.checkHandleValue(givenValue);
 
     if (this.isRange.get() === true) {
-      if (value > this.rightHandleValue.get()) value = this.rightHandleValue.get() as number
+      if (value > this.rightHandleValue.get()) value = this.rightHandleValue.get() as number;
     }
 
-    return value
+    return value;
   }
 
-  private checkRightHandleValue(value: number): number {
+  private checkRightHandleValue(givenValue: number): number {
+    let value = givenValue;
+
     if (this.isRange.get()) {
-      value = this.checkHandleValue(value)
-      if (value < this.leftHandleValue.get()) value = this.leftHandleValue.get() as number
+      value = this.checkHandleValue(givenValue);
+      if (value < this.leftHandleValue.get()) value = this.leftHandleValue.get() as number;
+    } else {
+      const defaultValuesSize = (this.defaultValues.get() as number[] | string[]).length;
+      if (this.hasDefaultValues.get()) value = defaultValuesSize - 1;
+      else value = this.maxValue.get() as number;
     }
-    else {
-      let defaultValuesSize = (this.defaultValues.get() as number[] | string[]).length
-      if (this.hasDefaultValues.get()) value = defaultValuesSize - 1
-      else value = this.maxValue.get() as number
-    }
-    return value
+
+    return value;
   }
 
-  private checkHandleValue(value: number): number {
+  private checkHandleValue(givenValue: number): number {
+    let value = givenValue;
+
     if (this.hasDefaultValues.get()) {
-      let defaultValuesSize = (this.defaultValues.get() as number[] | string[]).length
-      if (value < 0) value = 0
-      if (value > defaultValuesSize - 1) value = defaultValuesSize - 1
+      const defaultValuesSize = (this.defaultValues.get() as number[] | string[]).length;
+      if (value < 0) value = 0;
+      if (value > defaultValuesSize - 1) value = defaultValuesSize - 1;
+    } else {
+      const step = this.step.get() as number;
+      if (value % step !== 0) value -= value % step;
+      if (value < this.minValue.get()) value = this.minValue.get() as number;
+      if (value > this.maxValue.get()) value = this.maxValue.get() as number;
     }
-    else {
-      let step = this.step.get() as number
-      if (value % step != 0) value -= value % step
-      if (value < this.minValue.get()) value = this.minValue.get() as number
-      if (value > this.maxValue.get()) value = this.maxValue.get() as number
-    }
-    return value
+
+    return value;
   }
 }
 
-export { SliderConfig }
+export { SliderConfig };
--- src/slider/sliderConfig/sliderConfigItem.ts
+++ src/slider/sliderConfig/sliderConfigItem.ts
@@ -1,43 +1,46 @@
 interface ISliderConfigItem {
-  set(value: number | string | boolean | number[] | string[]): void
-  get(): number | string | boolean | number[] | string[]
+  set(value: number | string | boolean | number[] | string[]): void;
+  get(): number | string | boolean | number[] | string[];
 }
 
 class SliderConfigItem implements ISliderConfigItem {
-  private value: number | string | boolean | number[] | string[]
-  private subscribers: Function[] = new Array()
-  private checkValue: Function
+  private value: number | string | boolean | number[] | string[];
 
-  constructor(value: number | string | boolean | number[] | string[], checkValue?: Function) {
-    this.value = value
-    this.checkValue = checkValue ? checkValue : (value: number | string | boolean | number[] | string[]) => {return value}
+  private subscribers: Function[] = [];
+
+  private checkValue: Function;
+
+  constructor(
+    value: number | string | boolean | number[] | string[],
+    checkValue?: Function,
+  ) {
+    this.value = value;
+    this.checkValue = checkValue
+      || ((givenValue: number | string | boolean | number[] | string[]) => givenValue);
   }
 
   public set(value: number | string | boolean | number[] | string[]) {
-    value = this.checkValue(value)
-    this.value = value
-    this.publish(this.value)
+    this.value = this.checkValue(value);
+    this.publish(this.value);
   }
 
   public get(): number | string | boolean | number[] | string[] {
-    return this.value
+    return this.value;
   }
 
   public addSubscriber(subscriber: Function) {
-    this.subscribers.push(subscriber)
+    this.subscribers.push(subscriber);
   }
 
   public removeSubscriber(subscriber: Function) {
-    this.subscribers = this.subscribers.filter((el) => {
-      return el !== subscriber
-    })
+    this.subscribers = this.subscribers.filter((el) => el !== subscriber);
   }
 
   private publish(data: number | string | boolean | number[] | string[]) {
     this.subscribers.forEach((subscriber) => {
-      subscriber(data)
-    })
+      subscriber(data);
+    });
   }
 }
 
-export { SliderConfigItem }
+export { SliderConfigItem };
