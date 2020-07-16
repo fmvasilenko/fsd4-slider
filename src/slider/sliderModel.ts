9ea500a00e4d7ed44327befbc346c4774b4c0cfa
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-07-16 19:05:31 +0300

    fixed code style with eslint

--- src/slider/sliderModel.ts
+++ src/slider/sliderModel.ts
@@ -1,52 +1,52 @@
-import { SliderConfig } from "./sliderConfig/sliderConfig"
-import { SliderState } from "./sliderState/sliderState"
+import { SliderConfig } from './sliderConfig/sliderConfig';
+import { SliderState } from './sliderState/sliderState';
 
 class SliderModel {
-  private config: SliderConfig
-  private state: SliderState
+  private config: SliderConfig;
 
-  constructor (config: SliderConfig, state: SliderState) {
-    this.config = config
-    this.state = state
-    this.state.leftHandlePosition.addSubscriber(this.calculateLeftHandleValue.bind(this))
-    this.state.rightHandlePosition.addSubscriber(this.calculateRightHandleValue.bind(this))
+  private state: SliderState;
+
+  constructor(config: SliderConfig, state: SliderState) {
+    this.config = config;
+    this.state = state;
+    this.state.leftHandlePosition.addSubscriber(this.calculateLeftHandleValue.bind(this));
+    this.state.rightHandlePosition.addSubscriber(this.calculateRightHandleValue.bind(this));
   }
 
   private calculateLeftHandleValue(position: number): number {
-    if (this.config.hasDefaultValues.get()) this.config.leftHandleValue.set(this.calculateDefaultValue(position))
-    else this.config.leftHandleValue.set(this.calculateValue(position))
+    if (this.config.hasDefaultValues.get()) this.config.leftHandleValue.set(this.calculateDefaultValue(position));
+    else this.config.leftHandleValue.set(this.calculateValue(position));
 
-    return this.config.leftHandleValue.get() as number
+    return this.config.leftHandleValue.get() as number;
   }
 
   private calculateRightHandleValue(position: number): number {
-    if (this.config.hasDefaultValues.get()) this.config.rightHandleValue.set(this.calculateDefaultValue(position))
-    else this.config.rightHandleValue.set(this.calculateValue(position))
+    if (this.config.hasDefaultValues.get()) this.config.rightHandleValue.set(this.calculateDefaultValue(position));
+    else this.config.rightHandleValue.set(this.calculateValue(position));
 
-    return this.config.rightHandleValue.get() as number
+    return this.config.rightHandleValue.get() as number;
   }
 
   private calculateValue(position: number): number {
-    let minValue = this.config.minValue.get() as number
-    let maxValue = this.config.maxValue.get() as number
-    let step = this.config.step.get() as number
+    const minValue = this.config.minValue.get() as number;
+    const maxValue = this.config.maxValue.get() as number;
+    const step = this.config.step.get() as number;
 
-    let range = maxValue - minValue
-    let value = Math.floor(position * range)
+    const range = maxValue - minValue;
+    let value = Math.floor(position * range);
 
-    if (value % step > step / 2) value += step
-    value = value - value % step + minValue
+    if (value % step > step / 2) value += step;
+    value = value - (value % step) + minValue;
 
-    return value
+    return value;
   }
 
   private calculateDefaultValue(position: number): number {
-    if (this.config.defaultValues !== undefined) {
-      let defaultValuesLength = (this.config.defaultValues.get() as number[] | string[]).length
-      return Math.round((defaultValuesLength - 1) * position)
-    }
-    else return 0
+    const defaultValues = this.config.defaultValues.get() as number[] | string[];
+    return this.config.defaultValues !== undefined
+      ? Math.round((defaultValues.length - 1) * position)
+      : 0;
   }
 }
 
-export { SliderModel }
+export { SliderModel };
