1e384a29591a3845ddae843d7d4fce7d15726148
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-07-16 18:38:34 +0300

    fixed code style with eslint

--- src/slider/view/sliderValueLabelView.ts
+++ src/slider/view/sliderValueLabelView.ts
@@ -1,69 +1,72 @@
-import { SliderConfig } from "../sliderConfig/sliderConfig"
-enum Side{Left, Right}
+import { SliderConfig } from '../sliderConfig/sliderConfig';
+
+enum Side { Left, Right }
 
 class SliderValueLabelView {
-  private CONTAINER: HTMLElement
-  private config: SliderConfig
-  private SIDE: Side
-  private CLASSES: SliderClasses
-  private ROOT: HTMLElement
+  private CLASSES: SliderClasses;
+
+  private CONTAINER: HTMLElement;
+
+  private config: SliderConfig;
+
+  private SIDE: Side;
+
+  private ROOT: HTMLElement;
 
   constructor(container: HTMLElement, config: SliderConfig, side: Side) {
-    this.CONTAINER = container
-    this.config = config
-    this.SIDE = side
-    this.CLASSES = require("../sliderClasses.json")
-    this.ROOT = this.createRoot()
+    this.CLASSES = require('../sliderClasses.json');
+    this.CONTAINER = container;
+    this.config = config;
+    this.SIDE = side;
+    this.ROOT = this.createRoot();
 
-    this.switchVertical()
-    this.switch()
+    this.switchVertical();
+    this.switch();
 
-    this.config.isVertical.addSubscriber(this.switchVertical.bind(this))
-    this.config.valueLabelDisplayed.addSubscriber(this.switch.bind(this))
+    this.config.isVertical.addSubscriber(this.switchVertical.bind(this));
+    this.config.valueLabelDisplayed.addSubscriber(this.switch.bind(this));
 
     if (this.SIDE === Side.Left) {
-      this.updateValue(this.config.leftHandleValue.get() as number)
-      this.config.leftHandleValue.addSubscriber(this.updateValue.bind(this))
-    }
-    else {
-      this.updateValue(this.config.rightHandleValue.get() as number)
-      this.config.rightHandleValue.addSubscriber(this.updateValue.bind(this))
+      this.updateValue(this.config.leftHandleValue.get() as number);
+      this.config.leftHandleValue.addSubscriber(this.updateValue.bind(this));
+    } else {
+      this.updateValue(this.config.rightHandleValue.get() as number);
+      this.config.rightHandleValue.addSubscriber(this.updateValue.bind(this));
     }
   }
 
   private createRoot(): HTMLElement {
-    let root = document.createElement("div")
-    root.classList.add(this.CLASSES.VALUE_LABEL)
+    const root = document.createElement('div');
+    root.classList.add(this.CLASSES.VALUE_LABEL);
 
-    if (this.SIDE == Side.Right) root.classList.add(this.CLASSES.RIGHT_HANDLE_LABEL)
-    else root.classList.add(this.CLASSES.LEFT_HANDLE_LABEL)
+    if (this.SIDE === Side.Right) root.classList.add(this.CLASSES.RIGHT_HANDLE_LABEL);
+    else root.classList.add(this.CLASSES.LEFT_HANDLE_LABEL);
 
-    return root
+    return root;
   }
 
   private updateValue(value: number) {
     if (this.config.hasDefaultValues.get() === true) {
-      let defaultValues = this.config.defaultValues.get() as number[] | string[]
-      this.ROOT.innerHTML = `${defaultValues[value]}`
-    }
-    else this.ROOT.innerHTML = `${value}`
+      const defaultValues = this.config.defaultValues.get() as number[] | string[];
+      this.ROOT.innerHTML = `${defaultValues[value]}`;
+    } else this.ROOT.innerHTML = `${value}`;
   }
 
   private switchVertical() {
     if (this.config.isVertical.get() === true) {
-      if (this.SIDE === Side.Left) this.ROOT.classList.add(this.CLASSES.LEFT_HANDLE_LABEL_VERTICAL)
-      else this.ROOT.classList.add(this.CLASSES.RIGHT_HANDLE_LABEL_VERTICAL)
-    }
-    else {
-      if (this.SIDE === Side.Left) this.ROOT.classList.remove(this.CLASSES.LEFT_HANDLE_LABEL_VERTICAL)
-      else this.ROOT.classList.remove(this.CLASSES.RIGHT_HANDLE_LABEL_VERTICAL)
+      if (this.SIDE === Side.Left) this.ROOT.classList.add(this.CLASSES.LEFT_HANDLE_LABEL_VERTICAL);
+      else this.ROOT.classList.add(this.CLASSES.RIGHT_HANDLE_LABEL_VERTICAL);
+    } else {
+      // eslint-disable-next-line no-lonely-if
+      if (this.SIDE === Side.Left) this.ROOT.classList.remove(this.CLASSES.LEFT_HANDLE_LABEL_VERTICAL);
+      else this.ROOT.classList.remove(this.CLASSES.RIGHT_HANDLE_LABEL_VERTICAL);
     }
   }
 
   private switch() {
-    if (this.config.valueLabelDisplayed.get() === true) this.CONTAINER.appendChild(this.ROOT)
-    else this.ROOT.remove()
+    if (this.config.valueLabelDisplayed.get() === true) this.CONTAINER.appendChild(this.ROOT);
+    else this.ROOT.remove();
   }
 }
 
-export { SliderValueLabelView }
+export { SliderValueLabelView };
