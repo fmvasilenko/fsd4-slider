31e69eeeccb416b4d52d6c7d4529070f8d3d1695
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-07-17 11:33:10 +0300

    fixed code style with eslint

--- test/slider/sliderState/sliderState.ts
+++ test/slider/sliderState/sliderState.ts
@@ -1,44 +1,44 @@
-import { expect } from "chai"
-import { SliderState } from "../../../src/slider/sliderState/sliderState"
+import { expect } from 'chai';
+import { SliderState } from '../../../src/slider/sliderState/sliderState';
 
-describe("sliderState", () => {
-  describe("leftHandlePosition.set()", () => {
-    it("should set position to the given one", () => {
-      let sliderState = new SliderState(0.5, 0.5)
-      sliderState.leftHandlePosition.set(0)
-      expect(sliderState.leftHandlePosition.get()).to.equal(0)
-    })
+describe('sliderState', () => {
+  describe('leftHandlePosition.set()', () => {
+    it('should set position to the given one', () => {
+      const sliderState = new SliderState(0.5, 0.5);
+      sliderState.leftHandlePosition.set(0);
+      expect(sliderState.leftHandlePosition.get()).to.equal(0);
+    });
 
-    it("should change position to 0 if it`s lower then 0", () => {
-      let sliderState = new SliderState(0.5, 0.5)
-      sliderState.leftHandlePosition.set(-2)
-      expect(sliderState.leftHandlePosition.get()).to.equal(0)
-    })
+    it('should change position to 0 if it`s lower then 0', () => {
+      const sliderState = new SliderState(0.5, 0.5);
+      sliderState.leftHandlePosition.set(-2);
+      expect(sliderState.leftHandlePosition.get()).to.equal(0);
+    });
 
-    it("should change position to 1 if it`s higher then 1", () => {
-      let sliderState = new SliderState(0.5, 0.5)
-      sliderState.leftHandlePosition.set(2)
-      expect(sliderState.leftHandlePosition.get()).to.equal(1)
-    })
-  })
+    it('should change position to 1 if it`s higher then 1', () => {
+      const sliderState = new SliderState(0.5, 0.5);
+      sliderState.leftHandlePosition.set(2);
+      expect(sliderState.leftHandlePosition.get()).to.equal(1);
+    });
+  });
 
-  describe("rightHandlePosition.set()", () => {
-    it("should set position to the given one", () => {
-      let sliderState = new SliderState(0.5, 0.5)
-      sliderState.rightHandlePosition.set(0)
-      expect(sliderState.rightHandlePosition.get()).to.equal(0)
-    })
+  describe('rightHandlePosition.set()', () => {
+    it('should set position to the given one', () => {
+      const sliderState = new SliderState(0.5, 0.5);
+      sliderState.rightHandlePosition.set(0);
+      expect(sliderState.rightHandlePosition.get()).to.equal(0);
+    });
 
-    it("should change position to 0 if it`s lower then 0", () => {
-      let sliderState = new SliderState(0.5, 0.5)
-      sliderState.rightHandlePosition.set(-2)
-      expect(sliderState.rightHandlePosition.get()).to.equal(0)
-    })
+    it('should change position to 0 if it`s lower then 0', () => {
+      const sliderState = new SliderState(0.5, 0.5);
+      sliderState.rightHandlePosition.set(-2);
+      expect(sliderState.rightHandlePosition.get()).to.equal(0);
+    });
 
-    it("should change position to 1 if it`s higher then 1", () => {
-      let sliderState = new SliderState(0.5, 0.5)
-      sliderState.rightHandlePosition.set(2)
-      expect(sliderState.rightHandlePosition.get()).to.equal(1)
-    })
-  })
-})
+    it('should change position to 1 if it`s higher then 1', () => {
+      const sliderState = new SliderState(0.5, 0.5);
+      sliderState.rightHandlePosition.set(2);
+      expect(sliderState.rightHandlePosition.get()).to.equal(1);
+    });
+  });
+});
