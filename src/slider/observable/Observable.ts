2ff691a0a407c62e18f615d1f264eda6002e3dbe
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-11-04 20:31:35 +0300

    branch initial commit

---
+++ src/slider/observable/Observable.ts
@@ -0,0 +1,46 @@
+interface IObservable<T> {
+  set(value: T): void;
+  get(): T;
+}
+
+class Observable<T> implements IObservable<T> {
+  private value: T;
+
+  private subscribers: Function[] = [];
+
+  private checkValue: Function;
+
+  constructor(
+    value: T,
+    checkValue?: Function,
+  ) {
+    this.value = value;
+    this.checkValue = checkValue
+      || ((givenValue: T) => givenValue);
+  }
+
+  public set(value: T) {
+    this.value = this.checkValue(value);
+    this.publish(this.value);
+  }
+
+  public get(): T {
+    return this.value;
+  }
+
+  public addSubscriber(subscriber: Function) {
+    this.subscribers.push(subscriber);
+  }
+
+  public removeSubscriber(subscriber: Function) {
+    this.subscribers = this.subscribers.filter((el) => el !== subscriber);
+  }
+
+  private publish(data: T) {
+    this.subscribers.forEach((subscriber) => {
+      subscriber(data);
+    });
+  }
+}
+
+export { Observable };
