48c46ad1eb7baf02310806cff701c45652907616
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-11-12 18:22:07 +0300

    fixed build webpack config

--- config/webpack.build.config.js
+++ config/webpack.build.config.js
@@ -1,15 +1,10 @@
 const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.config');
-const libWebpackConfig = require('./webpack.lib.config');
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
   mode: 'production',
 });
 
-const buildLibWebpackConfig = merge(libWebpackConfig, {
-  mode: 'production',
-});
-
 module.exports = new Promise((resolve) => {
-  resolve([buildWebpackConfig, buildLibWebpackConfig]);
+  resolve(buildWebpackConfig);
 });
