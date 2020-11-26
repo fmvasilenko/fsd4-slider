d8701ca97425519fc6e0b1f096c400d00bce5afe
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-11-26 17:48:11 +0300

    moved demoSlider styles to demoSlider dir #11

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 /* eslint-disable no-plusplus */
 
 import './main.scss';
+import './demoSlider/demoSlider.scss';
 import { DemoSlider } from './demoSlider/DemoSlider';
 
 const pageWrapper = $('.page-wrapper');
